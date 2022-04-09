4682b4da24fdf6d2f43cd39536bae43860c93a71
yogeshsaraogi <yogeshsarowgi@gmail.com>
2022-04-09 13:42:55 +0530

    core component created

--- client/frontend/src/app/app.module.ts
+++ client/frontend/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './core/welcome/welcome.component';
+import { RegisterComponent } from './core/register/register.component';
+import { LoginComponent } from './core/login/login.component';
+import { BrowseProductComponent } from './core/browse-product/browse-product.component';
+import { CartComponent } from './core/cart/cart.component';
+import { CheckoutComponent } from './core/checkout/checkout.component';
+import { OrdersComponent } from './core/orders/orders.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    BrowseProductComponent,
+    CartComponent,
+    CheckoutComponent,
+    OrdersComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/frontend/src/app/core/browse-product/browse-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-browse-product',
+  templateUrl: './browse-product.component.html',
+  styleUrls: ['./browse-product.component.css']
+})
+export class BrowseProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
